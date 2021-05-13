d129c603c34904ae5b48e3620a748583b6659567
Vitalii-Horbenko <62204874+Vitalii-Horbenko@users.noreply.github.com>
2021-05-13 17:03:58 +0300

    hw1

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,38 @@
+import React from "react";
+import user from "../../user.json";
+import style from "./Profile.module.css";
+
+const { name, tag, location, avatar } = user;
+const { followers, views, likes } = user.stats;
+
+const Profile = () => {
+  return (
+    <div className={style.container}>
+      <div className={style.profile}>
+        <div className={style.description}>
+          <img src={avatar} alt={name} width="200" className={style.avatar} />
+          <p className={style.name}>{name}</p>
+          <p className={style.tag}>@{tag}</p>
+          <p className={style.location}>{location}</p>
+        </div>
+
+        <ul className={style.stats}>
+          <li className={style.profileItem}>
+            <span className={style.label}>Followers</span>
+            <span className={style.quantity}>{followers}</span>
+          </li>
+          <li className={style.profileItem}>
+            <span className={style.label}>Views</span>
+            <span className={style.quantity}>{views}</span>
+          </li>
+          <li className={style.profileItem}>
+            <span className={style.label}>Likes</span>
+            <span className={style.quantity}>{likes}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
