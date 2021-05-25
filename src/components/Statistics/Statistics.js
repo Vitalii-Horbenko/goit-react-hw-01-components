e18b02c0d9ecb94a598f76ebf728441625433b07
Vitalii-Horbenko <62204874+Vitalii-Horbenko@users.noreply.github.com>
2021-05-25 13:11:05 +0300

    fixed propTypes

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -10,7 +10,7 @@ const Statistics = ({ title, stats }) => {
   return (
     <div>
       <section className={statCss.statistics}>
-        {{title} && (<h2 className={statCss.title}>{title}</h2>)}
+        {{ title } && <h2 className={statCss.title}>{title}</h2>}
 
         <ul className={statCss.statList}>
           {stats.map(({ id, label, percentage }) => (
@@ -33,8 +33,13 @@ const Statistics = ({ title, stats }) => {
 
 Statistics.propTypes = {
   title: PropTypes.string,
-  stats: PropTypes.arrayOf(PropTypes.object).isRequired
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ).isRequired,
 };
-
 
 export default Statistics;
